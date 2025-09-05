72d7ba57c529b1b0f52a952134845f77891c218b
NaF <nfflourine@gmail.com>
2025-09-05 22:53:04 +0600

    Complete copy section

---
+++ copy.js
@@ -0,0 +1,107 @@
+const copyCount= document.getElementById('copy');
+
+
+document.getElementById('copy-btn').addEventListener('click',function(e){
+    e.preventDefault();
+
+    let count = parseInt(copyCount.innerText);
+    count++;
+    copyCount.innerText = count;
+    
+})
+
+
+
+
+document.getElementById('copy-btn1').addEventListener('click',function(e){
+    e.preventDefault();
+
+    let count = parseInt(copyCount.innerText);
+    count++;
+    copyCount.innerText = count;
+    
+})
+
+
+
+
+document.getElementById('copy-btn2').addEventListener('click',function(e){
+    e.preventDefault();
+
+    let count = parseInt(copyCount.innerText);
+    count++;
+    copyCount.innerText = count;
+    
+})
+
+
+
+
+document.getElementById('copy-btn3').addEventListener('click',function(e){
+    e.preventDefault();
+
+    let count = parseInt(copyCount.innerText);
+    count++;
+    copyCount.innerText = count;
+    
+})
+
+
+
+
+document.getElementById('copy-btn4').addEventListener('click',function(e){
+    e.preventDefault();
+
+    let count = parseInt(copyCount.innerText);
+    count++;
+    copyCount.innerText = count;
+    
+})
+
+
+
+
+document.getElementById('copy-btn5').addEventListener('click',function(e){
+    e.preventDefault();
+
+    let count = parseInt(copyCount.innerText);
+    count++;
+    copyCount.innerText = count;
+    
+})
+
+
+
+
+document.getElementById('copy-btn6').addEventListener('click',function(e){
+    e.preventDefault();
+
+    let count = parseInt(copyCount.innerText);
+    count++;
+    copyCount.innerText = count;
+    
+})
+
+
+
+
+document.getElementById('copy-btn7').addEventListener('click',function(e){
+    e.preventDefault();
+
+    let count = parseInt(copyCount.innerText);
+    count++;
+    copyCount.innerText = count;
+    
+})
+
+
+
+
+document.getElementById('copy-btn8').addEventListener('click',function(e){
+    e.preventDefault();
+
+    let count = parseInt(copyCount.innerText);
+    count++;
+    copyCount.innerText = count;
+    
+})
